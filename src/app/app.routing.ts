f17a81b6018441afda401ac9cb576bc2d76773a1
AldoxContreras <68927132+AldoxContreras@users.noreply.github.com>
2022-03-06 14:42:11 -0600

    Update app.routing.ts
    https://m.youtube.com/watch?v=ZoeZxpfTCXk
    
    Avance del archivo routing y creación del branch, y se agrega un link de un vídeotutorial de child routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,25 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+//import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes =[
+  {
+    path: '', redirectTo: '/iniciosesion', pathMatch: 'full',
+  }, 
+  { path: 'dashboard', component: DashboardComponent,
+    children: 
+  [
+    { path: 'user-profile', component: UserProfileComponent}
+  ]
+  }];
+// VÍDEO PARA GUIARSE CON LAS CHILD ROUTES: https://m.youtube.com/watch?v=ZoeZxpfTCXk
+
+
+
+/*const routes: Routes =[
   {
     path: '',
     redirectTo: 'dashboard',
@@ -18,7 +34,8 @@ const routes: Routes =[
       loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
     }]
   }
-];
+];*/
+
 
 @NgModule({
   imports: [

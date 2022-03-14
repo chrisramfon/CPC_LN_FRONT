0bac11ce92b5787dcfb05261f50259ac1f9e7a87
Christian Ramos <chrisramfon@outlook.com>
2022-03-13 20:32:14 -0600

    Se corrgie el html de verinfocliente

--- src/app/verinfocliente/verinfocliente.component.ts
+++ src/app/verinfocliente/verinfocliente.component.ts
@@ -1,7 +1,11 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
+//Import de la clase ActivatedRoute para obtener el id de la URL
 import { ActivatedRoute, Router } from '@angular/router';
+
+//Import del servicio del cliente
 import { ClienteService } from 'app/services/cliente.service';
+
 @Component({
   selector: 'app-verinfocliente',
   templateUrl: './verinfocliente.component.html',
@@ -9,43 +13,44 @@ import { ClienteService } from 'app/services/cliente.service';
 })
 export class VerinfoclienteComponent implements OnInit {
 
-  constructor(private router:Router, private route:ActivatedRoute, private ClienteServicio:ClienteService) {  }
+  //Se crean los objetos de las clases importadas
+  constructor(private router: Router, private route: ActivatedRoute, private ClienteServicio: ClienteService) { }
+
+  /* Arreglos dentro del cliente */
+  factura = [{ folio: "", adeudo: "", pagado: "", total: "", fecha_factura: "", fecha_limite: "", notas: "" }]
+  Cuenta = [{ cuenta: "", banco: "", estado_b: "" }]
+
+  /*Se declara al objeto Cliente*/ 
+  Cliente = { _id: "", nombre: "", rfc: "", telefono: "", correo: "", direccion: "", __v: "", factura: [this.factura], informacion_bancaria: [this.Cuenta] }
 
 
-  factura = [{
-    folio:"", adeudo:"", pagado:"", total:"", fecha_factura:"", fecha_limite:"", notas:"",
-}]
-  Cliente = { 
-     _id:"",nombre:"", rfc:"", telefono:"", correo:"", direccion:"", __v:"", factura:[this.factura]
-  }
-
- 
-  Cuenta = [{cuenta: "", banco: "", estado_b: ""}]
-
-  
-
-  Clientec = { _id:"",nombre:"", rfc:"", telefono:"", correo:"", direccion:"", __v:"", informacion_bancaria: [this.Cuenta] }
 
   ngOnInit(): void {
+
+    //Se ejecuta la función getCliente() cuando se entra al componente
     this.getCliente()
-  }
-  volveraListadeCliente(){
-    this.router.navigate(['admin/listacliente'])
-  }
-  getCliente(){
-    this.Cliente._id = this.route.snapshot.paramMap.get('id')
-    this.ClienteServicio.getverCliente(this.Cliente).subscribe(res=>{
-    this.Cliente = res;
-    console.log(this.Cliente.factura)
-      
-    this.Cliente = res; 
-    console.log(this.Cuenta)
-    }, err => {console.log(err)
-    console.log(err)
-    alert("Cliente no encontrado")
-    }
-    
-    )
+
   }
 
-}
+  //Regresa a a lista de clientes
+  volveraListadeCliente() {
+    this.router.navigate(['admin/listacliente'])
+  }
+
+  //Busca al cliente y lo guarda en el objeto Cliente
+  getCliente() {
+
+    //Obtiene el _id de la URL y lo guarda en Cliente._id
+    this.Cliente._id = this.route.snapshot.paramMap.get('id')
+    this.ClienteServicio.getverCliente(this.Cliente).subscribe(res => {
+
+      this.Cliente = res;
+      console.log(this.Cliente)
+
+    }, err => {
+      console.log(err)
+      alert("Cliente no encontrado")
+    }//Fin del err=>{}
+    )//Fin del subscribe
+  }//Fin del metodo getCliente()
+}//Fin de la clase

a85fc7f5d74776143a2ca1b452e58d8753d14028
Christian Ramos <chrisramfon@outlook.com>
2022-03-14 22:23:44 -0600

    Se agregan comentarios en lista de clientes

--- src/app/registrar-adeudo/registrar-adeudo.component.ts
+++ src/app/registrar-adeudo/registrar-adeudo.component.ts
@@ -7,42 +7,56 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './registrar-adeudo.component.html',
   styleUrls: ['./registrar-adeudo.component.css']
 })
+
 export class RegistrarAdeudoComponent implements OnInit {
 
-Adeudo = {
-  folio:"", adeudo:"", pagado:"", total:"", fecha_factura:"", fecha_limite:"", notas:"", _id:""
-}
+  //Se declara el objeto adeudo para ser llenado con los datos del formulario
+  Adeudo = {
+    folio: "", adeudo: "", pagado: "", total: "", fecha_factura: "", fecha_limite: "", notas: "", _id: ""
+  }
 
-  constructor(private Aroute:ActivatedRoute,private ClienteServicio:ClienteService, private route:Router) { }
+  constructor(private Aroute: ActivatedRoute, private ClienteServicio: ClienteService, private route: Router) { }
 
   ngOnInit(): void {
   }
-  volveraListadeCliente(){
+
+  //Función para regresar a la lista de clientes
+  volveraListadeCliente() {
     this.route.navigate(['admin/listacliente'])
   }
 
-  
-postadeudo(){
-if (this.Adeudo.adeudo=="" || this.Adeudo.fecha_factura=="" || this.Adeudo.fecha_limite=="" || this.Adeudo.total==""|| this.Adeudo.folio==""){
-  alert("Faltan datos requeridos.")
-  throw "Faltan datos requeridos."
-}
-this.Adeudo._id = this.Aroute.snapshot.paramMap.get('id')
-this.ClienteServicio.postAdeudos(this.Adeudo).subscribe(
-  res =>{
-    console.log(res)
-    alert("Adeudo registrado")
-    this.limpiar()
-  }, 
-  err=>{
-    alert("El adeudo no se pudo registrar.")
-    console.log(err)
-  })
-  
-}
-limpiar(){
-  this.Adeudo = { folio:"", adeudo:"", pagado:"", total:"", fecha_factura:"", fecha_limite:"", notas:"", _id:""}
-}
+
+  postadeudo() {
+
+    //Valida que los campos requeridos no estén vacios
+    if (this.Adeudo.adeudo == "" || this.Adeudo.fecha_factura == "" || this.Adeudo.fecha_limite == "" || this.Adeudo.total == "" || this.Adeudo.folio == "") {
+      alert("Faltan datos requeridos.")
+      throw "Faltan datos requeridos."
+    }
+
+    //Guarda el id que se recibe desde la URL
+    this.Adeudo._id = this.Aroute.snapshot.paramMap.get('id')
+
+    //Envía el objeto adeudo al back end
+    this.ClienteServicio.postAdeudos(this.Adeudo).subscribe(
+      res => {
+        console.log(res)
+        alert("Adeudo registrado")
+
+        //Ejecuta el método para limpiar el formulario
+        this.limpiar()
+      },
+      err => {
+        alert("El adeudo no se pudo registrar.")
+        console.log(err)
+      })
+
+  }//Fin del metodo postadeud
+
+  //Vacia los datos del formulario
+  limpiar() {
+    this.Adeudo = { folio: "", adeudo: "", pagado: "", total: "", fecha_factura: "", fecha_limite: "", notas: "", _id: "" }
+  }
 }
 
 
